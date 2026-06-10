1f3d15382b474e9c374fc9e5b46174b0f8e0dc71
Alex Collyer <alex.collyer@acol.dev>
2026-06-10 23:49:13 +0100

    🚧 Added code schema

--- server/src/content-types/index.ts
+++ server/src/content-types/index.ts
@@ -1,5 +1,7 @@
 import EventsSchema from './Events/schema.json';
+import CodeSchema from './Code/schema.json';
 
 export default {
   event: { schema: EventsSchema },
+  code: { schema: CodeSchema },
 };
